c30e21aa51e4c536421e6b376563652a250a734c
Pablo <p5.tacoronte@gmail.com>
2023-05-13 10:27:29 -0700

    fixes

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -9,7 +9,17 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 {
 	if (!tree)
 		return (0);
-	if (!tree->parent)
-		return (-1);
+	return (_depth(tree->parent));
+}
+
+/**
+ * _depth- calculates depth of a tree
+ * @tree: tree
+ * Return: trees depth
+ */
+size_t _depth(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
 	return (1 + binary_tree_depth(tree->parent));
 }
