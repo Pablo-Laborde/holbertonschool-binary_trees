19ba1247251b48bb6591ad5b03f2653c82e419bf
Pablo <p5.tacoronte@gmail.com>
2023-05-13 02:41:29 -0700

    updates

--- binary_trees.h
+++ binary_trees.h
@@ -75,6 +75,7 @@ size_t binary_tree_nodes(const binary_tree_t *tree);
 int binary_tree_balance(const binary_tree_t *tree);
 
 /* 15 */
+int binary_tree_is_full(const binary_tree_t *tree);
 
 /* 16 */
 
