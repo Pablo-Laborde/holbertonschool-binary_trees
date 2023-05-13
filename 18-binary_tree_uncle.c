31870d780a5f8298834b053a3d45e04e7ea3a4ca
Pablo <p5.tacoronte@gmail.com>
2023-05-13 10:18:51 -0700

    exercise 18

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,29 @@
+#include "binary_trees.h"
+
+binary_tree_t *binary_tree_sibling(binary_tree_t *node);
+
+/**
+ * binary_tree_uncle- returns sibling
+ * @node: tree
+ * Return: uncle
+ */
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+  if (!node || !node->parent)
+    return (NULL);
+  return (binary_tree_sibling(node->parent));
+}
+
+/**
+ * binary_tree_sibling- returns sibling
+ * @node: tree
+ * Return: sibling
+ */
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	if (!node || !node->parent)
+		return (NULL);
+	if (node->parent->right == node)
+		return (node->parent->left);
+	return (node->parent->right);
+}
