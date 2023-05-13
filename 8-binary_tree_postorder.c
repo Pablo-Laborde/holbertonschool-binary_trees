e18f3401b4d18f74494ca2584abbe07653e8a881
Pablo <p5.tacoronte@gmail.com>
2023-05-13 01:56:57 -0700

    exercise 8

---
+++ 8-binary_tree_postorder.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_postorder- goes through tree
+ * @tree: tree to go through
+ * @func: function
+ * Return: void
+ */
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree && func)
+	{
+		binary_tree_preorder(tree->left, func);
+		binary_tree_preorder(tree->right, func);
+		func(tree->n);
+	}
+}
