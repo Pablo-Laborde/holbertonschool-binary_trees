91e4d1017362bf7d22b1c1deff37399b5a397625
Pablo <p5.tacoronte@gmail.com>
2023-05-13 02:11:39 -0700

    update

--- binary_trees.h
+++ binary_trees.h
@@ -60,6 +60,7 @@ void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
 size_t binary_tree_height(const binary_tree_t *tree);
 
 /* 10 */
+size_t binary_tree_depth(const binary_tree_t *tree);
 
 /* 11 */
 
