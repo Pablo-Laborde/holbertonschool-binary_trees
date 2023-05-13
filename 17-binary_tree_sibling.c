66d22213521c993546114c0338bf9dccae9416b3
Pablo <p5.tacoronte@gmail.com>
2023-05-13 10:08:09 -0700

    exercise 17

---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_sibling- returns sibling
+ * @node: tree
+ * Return: sibling
+ */
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	if (!tree || !node->parent)
+		return (NULL);
+  if (node->parent->right == node)
+    return (node->parent->left);
+  return (node->parent->right);
+}
