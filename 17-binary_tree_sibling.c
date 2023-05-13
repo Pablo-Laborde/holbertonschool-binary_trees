74267563ced7a0521d6818b7f1cffdea2dbced2d
Pablo <p5.tacoronte@gmail.com>
2023-05-13 10:09:44 -0700

    fix

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -7,7 +7,7 @@
  */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-	if (!tree || !node->parent)
+	if (!node || !node->parent)
 		return (NULL);
   if (node->parent->right == node)
     return (node->parent->left);
