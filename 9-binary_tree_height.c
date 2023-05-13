6157fc8c984db16b16909b12930145fd5be3c9be
Pablo <p5.tacoronte@gmail.com>
2023-05-13 02:02:08 -0700

    exercise 9

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height- calculates height of a tree
+ * @tree: tree 
+ * Return: trees height
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t sl, sr;
+
+	if (!tree)
+		return (0);
+	sl = binary_tree_height(tree->left);
+	sr = binary_tree_height(tree->right);
+	return (sl >= sr) ? (1 + sl) : (1 + sr);
+}
