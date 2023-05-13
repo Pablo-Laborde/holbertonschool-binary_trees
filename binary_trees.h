984d25948ff1fcf958b8ed6f27330fc434ed6bce
Pablo <p5.tacoronte@gmail.com>
2023-05-13 01:57:04 -0700

    update

--- binary_trees.h
+++ binary_trees.h
@@ -51,8 +51,10 @@ int binary_tree_is_root(const binary_tree_t *node);
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
 
 /* 7 */
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
 
 /* 8 */
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
 
 /* 9 */
 
