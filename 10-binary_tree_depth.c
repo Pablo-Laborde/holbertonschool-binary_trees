c4933797d7a414eeb045654aab7fdb335e530ae3
Pablo <p5.tacoronte@gmail.com>
2023-05-13 02:13:18 -0700

    fixes

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -1,14 +1,12 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_depth- calculates height of a tree
+ * binary_tree_depth- calculates depth of a tree
  * @tree: tree 
  * Return: trees height
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-	size_t sl, sr;
-
 	if (!tree)
 		return (0);
 	return (1 + binary_tree_height(tree->parent));
