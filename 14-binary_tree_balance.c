1c2f3a64243a5f9d177075630d09521be7a84f1e
Pablo <p5.tacoronte@gmail.com>
2023-05-13 09:39:59 -0700

    fix

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -11,7 +11,8 @@ int binary_tree_balance(const binary_tree_t *tree)
 {
 	if (!tree)
 		return (0);
-  return ((int)(binary_tree_height(tree->left) - binary_tree_height(tree->right)));
+	return ((int)(binary_tree_height(tree->left) -
+								binary_tree_height(tree->right)));
 }
 
 /**
