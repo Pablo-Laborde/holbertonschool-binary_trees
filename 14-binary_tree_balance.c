c29fc26d0cf23efc480f64c636a2e6928b990b43
Pablo <p5.tacoronte@gmail.com>
2023-05-13 09:36:50 -0700

    add 9

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,5 +1,7 @@
 #include "binary_trees.h"
 
+size_t binary_tree_height(const binary_tree_t *tree);
+
 /**
  * binary_tree_balance- calculates the difference
  * @tree: tree
@@ -10,4 +12,22 @@ int binary_tree_balance(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
   return ((int)(binary_tree_height(tree->left) - binary_tree_height(tree->right)));
+}
+
+/**
+ * binary_tree_height- calculates height of a tree
+ * @tree: tree
+ * Return: trees height
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t sl, sr;
+
+	if (!tree)
+		return (0);
+	sl = binary_tree_height(tree->left);
+	sr = binary_tree_height(tree->right);
+	if (!tree->parent)
+		return ((sl >= sr) ? (sl) : (sr));
+	return ((sl >= sr) ? (1 + sl) : (1 + sr));
 }
