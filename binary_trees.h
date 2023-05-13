dbd6e2d529ed4003eec2f3df23831f4050e423f7
Pablo <p5.tacoronte@gmail.com>
2023-05-13 01:51:53 -0700

    update

--- binary_trees.h
+++ binary_trees.h
@@ -48,5 +48,34 @@ int binary_tree_is_leaf(const binary_tree_t *node);
 int binary_tree_is_root(const binary_tree_t *node);
 
 /* 6 */
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
+
+/* 7 */
+
+/* 8 */
+
+/* 9 */
+
+/* 10 */
+
+/* 11 */
+
+/* 12 */
+
+/* 13 */
+
+/* 14 */
+
+/* 15 */
+
+/* 16 */
+
+/* 17 */
+
+/* 18 */
+
+/* 19 */
+
+/* 20 */
 
 #endif
