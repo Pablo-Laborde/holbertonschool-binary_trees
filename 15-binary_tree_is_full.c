414516e97964733c6a06f815172fce46a153a191
Pablo <p5.tacoronte@gmail.com>
2023-05-13 02:41:24 -0700

    exercise 15

---
+++ 15-binary_tree_is_full.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_full- checks if tree is full
+ * @tree: tree
+ * Return: 1 if true, 0 otherwise
+ */
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+  if (!tree->left && !tree->right)
+    return (1);
+  return (binary_tree_is_full(tree->left) * binary_tree_is_full(tree->right));
+}
